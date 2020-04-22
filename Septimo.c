98767e708c5fd89f34ba03cfedcd2cfa291cb175
mendezluis830 <54092388+mendezluis830@users.noreply.github.com>
2020-04-21 22:57:57 -0500

    Update Septimo.c

--- Septimo.c
+++ Septimo.c
@@ -1,30 +1,11 @@
-#include <math.h>
-#include <fstream>
-#include <iostream>
+#include <stdio.h>
+typedef struct_complex complex;
+  complex{
+    int real;
+    int ima;
+  };
 
-using namespace std;
+comlpex *c, complex1;
+c= &complex1;
 
-struct complejo
-{
-double Im; // parte imaginaria 
-double Re; // parte real
-};
-
-typedef struct complejo COMPLEJO;
-
-COMPLEJO * crea_estructura(void)
-{
-
-COMPLEJO *ES_T;
-ES_T = new (COMPLEJO);
-
-cout<<"Dame la parte real del numero complejo: "<<endl;
-cin>>ES_T->Re;
-cout<<"Dame la parte imaginaria del numero complejo: "<<endl;
-cin>>ES_T->Im;
-return(ES_T);
-}
-Una posible llamada a esta función seria:
-
-COMPLEJO * ES; 
-ES = crea_estructura();
+return 0;
